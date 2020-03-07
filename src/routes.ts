ca962164d43a7b7d27e30b977b8dc99612a67dee
Victor Marinho <victor.marinho@hipr.com.br>
2020-03-07 16:34:58 -0300

    files

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserControler from './app/controllers/UserControler';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth.middleware';
+import fileController from './app/controllers/fileController';
 
 declare global {
     namespace Express {
@@ -11,11 +16,14 @@ declare global {
     }
 }
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserControler.store);
 
 routes.put('/users', authMiddleware, UserControler.update);
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/files',fileController.store, authMiddleware, upload.single('file'))
 
 export default routes;
