794385c4825bc39e3406846c12f9d6c91b006a51
Victor Marinho <victor.marinho@hipr.com.br>
2020-03-08 03:13:57 -0300

    Marcar notificação como lida

--- src/routes.ts
+++ src/routes.ts
@@ -39,5 +39,6 @@ routes.get('/appointments', authMiddleware, AppointmentController.index);
 routes.get('/schedule', authMiddleware, ScheduleController.index);
 
 routes.get('/notifications',authMiddleware, NotificationController.index);
+routes.put('/notifications/:id',authMiddleware, NotificationController.update);
 
 export default routes;
