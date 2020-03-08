164da59844640089ce8893ec919f33fbe9b30355
Victor Marinho <victor.marinho@hipr.com.br>
2020-03-08 18:34:00 -0300

    Configurando fila com RADIS

--- src/app/controllers/AppointmentController.ts
+++ src/app/controllers/AppointmentController.ts
@@ -6,8 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 import  ptBR  from "date-fns/locale/pt-BR";
-import MailLib from '../../lib/Mail';
-import Mail from "../../lib/Mail";
+import Queue from "../../lib/Queue";
+import CancellationMail from "../jobs/CancellationMail";
 class AppointmentController {
 
     async index(req: Request, res: Response){
@@ -142,21 +142,9 @@ class AppointmentController {
 
         await appointment.save();
 
-        await Mail.sendMail({
-            to: `${appointment?.provider?.name} <${appointment?.provider?.email}>`,
-            subject: 'Agendamento cancelado',
-            template: 'cancelations',
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    appointment.date,
-                    "'dia' dd 'de' MMMM', às' H:mm'h'",
-                    {locale: ptBR}
-                )
-            }
+        await Queue.add(CancellationMail.key, {
+            appointment
         });
-
         return res.json(appointment);
     }
 }
